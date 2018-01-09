48008b9c4940347812253ab8338dd6a86b36f75e
Jack N <jacknagle22@gmail.com>
2018-01-09 19:20:18 +1100

    Updated gitignore and npmignore

--- tests/test.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('../src/index');
-const client = new Client();
-// client.user("272689325521502208").then(data => console.log(data)).catch(err => console.error(err));
-client.guildLeaderboard('303195322514014210').then(data => {
-    const sorted = data.filter(d => d).sort((a, b) => b.score - a.score);
-    console.log(sorted);
-}).catch(err => console.error(err));
